8c4fd1cad3293e6dc0bfeba685ad6916a7be642c
shivam yadav <91057727+Shivamycodee@users.noreply.github.com>
2023-05-02 19:42:42 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -171,13 +171,13 @@ function TopBar() {
                 >
                   <span className="sp">Change Profile</span>
                 </button>
-                <button
-                  style={{ width: "100%" }}
-                  data-label="Register"
-                  className="rainbow-hover"
-                >
-                  <span className="sp">Share</span>
-                </button>
+//                 <button
+//                   style={{ width: "100%" }}
+//                   data-label="Register"
+//                   className="rainbow-hover"
+//                 >
+//                   <span className="sp">Share</span>
+//                 </button>
               </div>
             </div>
           </div>
