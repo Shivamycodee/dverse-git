d6647811a19c487811cf5fcf889c3c617ddad01a
Shivamycodee <91057727+Shivamycodee@users.noreply.github.com>
2023-07-27 19:20:57 +0530

    script position change
    google analytics script position change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -10,23 +11,29 @@ class MyDocument extends Document {
             src="https://kit.fontawesome.com/ccd84e7d2b.js"
             crossOrigin="anonymous"
           />
-
-
-    {/*Google analytics... */ }
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0VDJC5JMTV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-0VDJC5JMTV');
-</script>
-
-
         </Head>
         <body>
           <Main />
+
+          {/* Google Analytics */}
+          <Script
+            id="google-data01"
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-0VDJC5JMTV`}
+          />
+          <Script
+            id="google-data02"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){window.dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-0VDJC5JMTV');
+              `,
+            }}
+          />
+
           <NextScript />
         </body>
       </Html>
